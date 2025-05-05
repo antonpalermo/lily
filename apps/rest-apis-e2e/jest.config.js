ffa2b87d0e8e95293a0dc67d020e2615b356b2e1
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-05-06 00:02:12 +0800

    wip: rest-apis e2e

--- apps/rest-apis-e2e/jest.config.js
+++ apps/rest-apis-e2e/jest.config.js
@@ -1,13 +1,13 @@
 export default {
   displayName: 'rest-apis-e2e',
   preset: '../../jest.preset.js',
-  globalSetup: '<rootDir>/src/support/global-setup.ts',
-  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
-  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  globalSetup: '<rootDir>/src/support/global-setup.js',
+  globalTeardown: '<rootDir>/src/support/global-teardown.js',
+  setupFiles: ['<rootDir>/src/support/test-setup.js'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': [
-      'ts-jest',
+      'babel-jest',
       {
         tsconfig: '<rootDir>/tsconfig.spec.json',
       },
--- apps/rest-apis-e2e/src/rest-apis/rest-apis.spec.js
+++ apps/rest-apis-e2e/src/rest-apis/rest-apis.spec.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-describe('GET /', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/`);
+describe('GET /api/health', () => {
+  it('should return a healthy response', async () => {
+    const res = await axios.get(`/api/health`);
     expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
+    expect(res.data).toHaveProperty('uptime');
+    expect(res.data).toHaveProperty('status', 'healthy');
   });
 });
