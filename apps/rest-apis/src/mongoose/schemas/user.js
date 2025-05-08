d2163cd0a7c81895d0b85c9e4a89fe9e5ed8ed47
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-05-08 22:44:36 +0800

    added timestamps

--- apps/rest-apis/src/mongoose/schemas/user.js
+++ apps/rest-apis/src/mongoose/schemas/user.js
@@ -1,18 +1,34 @@
 import mongoose from 'mongoose';
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: mongoose.Schema.Types.String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: mongoose.Schema.Types.String,
+      required: true,
+    },
+    username: {
+      type: mongoose.Schema.Types.String,
+      required: true,
+    },
+    password: {
+      type: mongoose.Schema.Types.String,
+      required: true,
+    },
   },
-  username: {
-    type: mongoose.Schema.Types.String,
-    required: true,
-  },
-  password: {
-    type: mongoose.Schema.Types.String,
-    required: true,
-  },
-});
+  {
+    timestamps: {
+      createdAt: 'dateCreated',
+      updatedAt: 'dateUpdated',
+    },
+    toJSON: {
+      virtuals: true,
+      transform: function (_, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 export const User = mongoose.model('users', UserSchema);
