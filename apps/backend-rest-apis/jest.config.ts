4f1af28deab59ed86dd8bba8ddb1a666efdafdd1
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-05-08 23:26:35 +0800

    initialize new nest project

---
+++ apps/backend-rest-apis/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'backend-rest-apis',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/backend-rest-apis',
+};
